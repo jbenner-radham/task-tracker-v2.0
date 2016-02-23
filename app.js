3fcf99ede0c85844038bfbae136c791fc5bffddb
James Benner <james.benner@gmail.com>
2016-02-22 21:43:20 -0500

    Working app.

---
+++ app.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict'
+
+    function addTask(task) {
+        var table = document.querySelector('table')
+        var row   = table.insertRow()
+
+        for (var prop in task) {
+            var val = task[prop]
+            var child = document.createTextNode(val)
+
+            row.insertCell().appendChild(child)
+        }
+    }
+
+    var json = document.getElementById('data').textContent
+    var data = JSON.parse(json)
+
+    data.forEach(function (task) {
+        addTask(task)
+    });
+
+    document.querySelector('form').addEventListener('click', function () {
+        var task = {
+            name:     this[0].value,
+            date:     this[1].value,
+            assigned: this[2].value
+        }
+
+        console.log(task)
+
+        addTask(task)
+    })
+})()
