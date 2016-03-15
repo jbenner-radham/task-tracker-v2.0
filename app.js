a3f2687efe68dcef9e5992186ee5456c1a533c5f
James Benner <james.benner@gmail.com>
2016-03-14 22:33:00 -0400

    Added form validation and clearing.

--- app.js
+++ app.js
@@ -31,12 +31,24 @@
     data.forEach(addTask)
 
     document.querySelector('button').addEventListener('click', function () {
-        var task = {
+        var dateRegEx = /[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}/
+        var task      = {
             name:     this.form.elements[0].value,
             date:     this.form.elements[1].value,
             assigned: this.form.elements[2].value
         }
 
+        // Validate the form input.
+        if (!task.name || !task.date || !task.assigned || !task.date.match(dateRegEx) ) {
+            return false
+        }
+
         prependTask(task)
+
+        // Clear the form values after creating the task.
+        for (var element of this.form.elements) {
+            console.log(element)
+            element.value = ''
+        }
     })
 })()
